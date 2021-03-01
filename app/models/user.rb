add596f4b974fd058bbf1990851215da6a5e4c98
kensuke-iwata <kensukeiwata1313@gmail.com>
2021-03-01 14:25:56 +0900

    Userモデルにバリデーションを設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :
+  validates :nickname,            presence: true
+  validates :encrypted_password,  presence: true
+  validates :family_name,         presence: true
+  validates :first_name,          presence: true
+  validates :family_name_kana,    presence: true
+  validates :first_name,          presence: true
+
+  has_many :items
+  has_many :buy
 end
